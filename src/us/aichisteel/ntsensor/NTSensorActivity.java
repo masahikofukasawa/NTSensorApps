046261722914fb642d210b760221e0f705b44de5
masahikofukasawa <fukasawa@aichi-steel.us>
2014-06-16 17:34:32 -0700

    restructure

--- src/us/aichisteel/ntsensor/NTSensorActivity.java
+++ src/us/aichisteel/ntsensor/NTSensorActivity.java
@@ -16,22 +16,15 @@
 
 package us.aichisteel.ntsensor;
 
-import java.io.IOException;
 import java.util.ArrayList;
-//import java.util.Arrays;
 import java.util.List;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.Bundle;
-import android.os.Handler;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.Paint.Align;
-import android.hardware.usb.UsbManager;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.Menu;
@@ -46,11 +39,6 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import us.aichisteel.ntsensor.R;
-
-import com.physicaloid.lib.Physicaloid;
-import com.physicaloid.lib.usb.driver.uart.UartConfig;
-
 import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
 import org.achartengine.chart.PointStyle;
@@ -59,31 +47,20 @@ import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 
-public class NTSensorActivity extends Activity {
+import us.aichisteel.amisensor.*;
+import us.aichisteel.ntsensor.R;
 
-	private NTSensorData sensorData = new NTSensorData();
+public class NTSensorActivity extends Activity implements
+AMISensorInterface {
 
-	Handler mHandler = new Handler();
-
-	private static final String CR = "\r";
-	private String stTransmit = CR;
-	private String stStartCommand = "a";
-	private String stStopCommand = "s";
-
-	Physicaloid mSerial;
-
+	private NTSensor mSensor;
 	private Button btStart;
 	private Button btStop;
 	private Button btOffset;
-	private StringBuilder mText = new StringBuilder();
-	private int iBaudRate = 115200;
 	private RadioGroup rgSensorSelection;
 	private RadioButton rbSensorSelectionNT;
 	private RadioButton rbSensorSelectionUT;
 
-	private boolean mRunningMainLoop;
-
-	private static final String ACTION_USB_PERMISSION = "us.aichisteel.ntsensor.USB_PERMISSION";
 
 	private XYMultipleSeriesRenderer mRenderer;
 	private XYMultipleSeriesDataset dataset;
@@ -91,8 +68,6 @@ public class NTSensorActivity extends Activity {
 
 	private double mXaxisMax = 5.0;
 
-	// Linefeed
-	private final static String BR = System.getProperty("line.separator");
 
 	private XYMultipleSeriesDataset buildDataset(String[] titles,
 			List<double[]> xValues, List<double[]> yValues) {
@@ -198,8 +173,6 @@ public class NTSensorActivity extends Activity {
 		layout.addView(graphicalView, new LayoutParams(
 				LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
 
-		mRunningMainLoop = false;
-
 		btStart = (Button) findViewById(R.id.btStart);
 		btStop = (Button) findViewById(R.id.btStop);
 		btOffset = (Button) findViewById(R.id.btOffset);
@@ -209,30 +182,24 @@ public class NTSensorActivity extends Activity {
 		rbSensorSelectionUT = (RadioButton) findViewById(R.id.rd_ut);
 
 		btOffset.setVisibility(View.GONE);
+
+		mSensor = new NTSensor(this);
+		mSensor.initialize(this);
+		enableButtons(mSensor.isReady());
+
 		rgSensorSelection.setVisibility(View.GONE);
-
-		// get service
-		mSerial = new Physicaloid(this);
-
-		// listen for new devices
-		IntentFilter filter = new IntentFilter();
-		filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
-		filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
-		registerReceiver(mUsbReceiver, filter);
-
-		openUsbSerial();
 
 		btStart.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				startSensor();
+				mSensor.startSensor();
 			}
 		});
 
 		btStop.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				stopSensor();
+				mSensor.stopSensor();
 			}
 		});
 
@@ -253,9 +220,36 @@ public class NTSensorActivity extends Activity {
 
 	@Override
 	public void onDestroy() {
-		mSerial.close();
-		unregisterReceiver(mUsbReceiver);
+		mSensor.finalize(this);
 		super.onDestroy();
+	}
+	@Override
+	public void onNewIntent(Intent intent) {
+		mSensor.initialize(this);
+		enableButtons(mSensor.isReady());
+	}
+
+	@Override
+	public void attachedSensor() {
+		enableButtons(mSensor.isReady());
+	}
+
+	@Override
+	public void detachedSensor() {
+		enableButtons(mSensor.isReady());
+		finish();
+	}
+
+	@Override
+	public void dataReady() {
+		mSensor.setMaxTime(mRenderer.getXAxisMax());
+		double time = 0;
+		dataset.getSeriesAt(0).clear();
+		for (Double data : mSensor.getData()) {
+			dataset.getSeriesAt(0).add(time, data);
+			time += (1.0 / NTSensor.NTSENSOR_SPS);
+		}
+		graphicalView.repaint();
 	}
 
 	private void enableButtons(boolean enable) {
@@ -269,161 +263,13 @@ public class NTSensorActivity extends Activity {
 		if (enable)
 			color = getResources().getColor(R.color.dark_blue);
 		else
-			color = Color.LTGRAY;
+			color = Color.GRAY;
 
 		btStart.setTextColor(color);
 		btStop.setTextColor(color);
 		btOffset.setTextColor(color);
 		rbSensorSelectionNT.setTextColor(color);
 		rbSensorSelectionUT.setTextColor(color);
-	}
-
-	private void openUsbSerial() {
-		if (mSerial == null) {
-			enableButtons(false);
-			return;
-		}
-
-		if (!mSerial.isOpened()) {
-			if (!mSerial.open()) {
-				enableButtons(false);
-				return;
-			} else {
-				mSerial.setConfig(new UartConfig(iBaudRate, 8, 1, 0, false,
-						false));
-				enableButtons(true);
-			}
-		}
-
-		if (!mRunningMainLoop) {
-			mainloop();
-		}
-	}
-
-	private void mainloop() {
-		mRunningMainLoop = true;
-		new Thread(mLoop).start();
-	}
-
-	public int mDataCount = 0;
-	public double mTime = 0.0;
-
-	private Runnable mLoop = new Runnable() {
-		@Override
-		public void run() {
-			int len;
-			byte[] rbuf = new byte[4096];
-
-			for (;;) {
-				len = mSerial.read(rbuf);
-
-				if (len > 0) {
-					sensorData.addData(rbuf, len);
-					sensorData.setMaxTime(mRenderer.getXAxisMax());
-					setSerialDataToTextView(rbuf, len);
-
-					mHandler.post(new Runnable() {
-						@Override
-						public void run() {
-							try {
-								double time = 0;
-								dataset.getSeriesAt(0).clear();
-								for (Double data : sensorData.getData()) {
-									dataset.getSeriesAt(0).add(time, data);
-									time += (1.0 / NTSensorData.NTSENSOR_SPS);
-								}
-								graphicalView.repaint();
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
-						}
-					});
-				}
-				try {
-					Thread.sleep(150);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-
-				if (!mRunningMainLoop) {
-					return;
-				}
-			}
-		}
-	};
-
-	boolean lastDataIs0x0D = false;
-
-	void setSerialDataToTextView(byte[] rbuf, int len) {
-		for (int i = 0; i < len; i++) {
-			// "\r":CR(0x0D) "\n":LF(0x0A)
-			if (rbuf[i] == 0x0D) {
-				mText.append(BR);
-			} else if (rbuf[i] == 0x0A) {
-				// if (iTarget != MENU_TARGET_NTSENSOR) {
-				mText.append(BR);
-				// }
-			} else if ((rbuf[i] == 0x0D) && (rbuf[i + 1] == 0x0A)) {
-				mText.append(BR);
-				i++;
-			} else if (rbuf[i] == 0x0D) {
-				// case of rbuf[last] == 0x0D and rbuf[0] == 0x0A
-				lastDataIs0x0D = true;
-			} else if (lastDataIs0x0D && (rbuf[0] == 0x0A)) {
-				mText.append(BR);
-				lastDataIs0x0D = false;
-			} else if (lastDataIs0x0D && (i != 0)) {
-				// only disable flag
-				lastDataIs0x0D = false;
-				i--;
-			} else {
-				mText.append((char) rbuf[i]);
-			}
-		}
-	}
-
-	private void closeUsbSerial() {
-		mSerial.close();
-	}
-
-	protected void onNewIntent(Intent intent) {
-		openUsbSerial();
-	};
-
-	private void detachedUi() {
-		enableButtons(false);
-	}
-
-	private String changeEscapeSequence(String in) {
-		String out = new String();
-		try {
-			out = unescapeJava(in);
-		} catch (IOException e) {
-			return "";
-		}
-
-		out = out + stTransmit;
-		return out;
-	}
-
-	private void startSensor() {
-		openUsbSerial();
-		if (mSerial.isOpened()) {
-			String strWrite = changeEscapeSequence(stStartCommand);
-			mSerial.write(strWrite.getBytes(), strWrite.length());
-			sensorData.initData();
-		} else {
-		}
-	}
-
-	private void stopSensor() {
-		if (mSerial.isOpened()) {
-			String strWrite = changeEscapeSequence(stStopCommand);
-			mSerial.write(strWrite.getBytes(), strWrite.length());
-			sensorData.initData();
-		} else {
-		}
-		closeUsbSerial();
 	}
 
 	private void setSensor(int id) {
@@ -435,149 +281,9 @@ public class NTSensorActivity extends Activity {
 	}
 
 	private void setOffset() {
-		double offset = sensorData.setOffset();
+		double offset = mSensor.setOffset();
 		Toast.makeText(this, "Offset=" + String.valueOf(offset) + "[V]",
 				Toast.LENGTH_SHORT).show();
-	}
-
-	BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
-		public void onReceive(Context context, Intent intent) {
-			String action = intent.getAction();
-
-			if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
-				if (!mSerial.isOpened()) {
-					openUsbSerial();
-				}
-				if (!mRunningMainLoop) {
-					mainloop();
-				}
-			} else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
-				detachedUi();
-				mSerial.close();
-				finish();
-			} else if (ACTION_USB_PERMISSION.equals(action)) {
-				synchronized (this) {
-					if (!mSerial.isOpened()) {
-						openUsbSerial();
-					}
-				}
-				if (!mRunningMainLoop) {
-					mainloop();
-				}
-			}
-		}
-	};
-
-	/**
-	 * <p>
-	 * Unescapes any Java literals found in the <code>String</code> to a
-	 * <code>Writer</code>.
-	 * </p>
-	 *
-	 * <p>
-	 * For example, it will turn a sequence of <code>'\'</code> and
-	 * <code>'n'</code> into a newline character, unless the <code>'\'</code> is
-	 * preceded by another <code>'\'</code>.
-	 * </p>
-	 *
-	 * <p>
-	 * A <code>null</code> string input has no effect.
-	 * </p>
-	 *
-	 * @param out
-	 *            the <code>String</code> used to output unescaped characters
-	 * @param str
-	 *            the <code>String</code> to unescape, may be null
-	 * @throws IllegalArgumentException
-	 *             if the Writer is <code>null</code>
-	 * @throws IOException
-	 *             if error occurs on underlying Writer
-	 */
-	private String unescapeJava(String str) throws IOException {
-		if (str == null) {
-			return "";
-		}
-		int sz = str.length();
-		StringBuffer unicode = new StringBuffer(4);
-
-		StringBuilder strout = new StringBuilder();
-		boolean hadSlash = false;
-		boolean inUnicode = false;
-		for (int i = 0; i < sz; i++) {
-			char ch = str.charAt(i);
-			if (inUnicode) {
-				// if in unicode, then we're reading unicode
-				// values in somehow
-				unicode.append(ch);
-				if (unicode.length() == 4) {
-					// unicode now contains the four hex digits
-					// which represents our unicode character
-					try {
-						int value = Integer.parseInt(unicode.toString(), 16);
-						strout.append((char) value);
-						unicode.setLength(0);
-						inUnicode = false;
-						hadSlash = false;
-					} catch (NumberFormatException nfe) {
-						// throw new
-						// NestableRuntimeException("Unable to parse unicode value: "
-						// + unicode, nfe);
-						throw new IOException("Unable to parse unicode value: "
-								+ unicode, nfe);
-					}
-				}
-				continue;
-			}
-			if (hadSlash) {
-				// handle an escaped value
-				hadSlash = false;
-				switch (ch) {
-				case '\\':
-					strout.append('\\');
-					break;
-				case '\'':
-					strout.append('\'');
-					break;
-				case '\"':
-					strout.append('"');
-					break;
-				case 'r':
-					strout.append('\r');
-					break;
-				case 'f':
-					strout.append('\f');
-					break;
-				case 't':
-					strout.append('\t');
-					break;
-				case 'n':
-					strout.append('\n');
-					break;
-				case 'b':
-					strout.append('\b');
-					break;
-				case 'u': {
-					// uh-oh, we're in unicode country....
-					inUnicode = true;
-					break;
-				}
-				default:
-					strout.append(ch);
-					break;
-				}
-				continue;
-			} else if (ch == '\\') {
-				hadSlash = true;
-				continue;
-			}
-			strout.append(ch);
-		}
-		if (hadSlash) {
-			// then we're in the weird case of a \ at the end of the
-			// string, let's output it anyway.
-			strout.append('\\');
-		}
-		return new String(strout.toString());
 	}
 
 	private enum MenuId {
@@ -636,7 +342,7 @@ public class NTSensorActivity extends Activity {
 		case DISPLAY_TIME_5SEC:
 		case DISPLAY_TIME_10SEC:
 			int time = Integer.parseInt(item.getTitle().toString());
-			sensorData.setMaxTime(time);
+			mSensor.setMaxTime(time);
 			mRenderer.setXAxisMax(time);
 			item.setChecked(item.isChecked() ? false : true);
 			break;
